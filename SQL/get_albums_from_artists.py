815817f7c24c3a5ae73753e4871347a8e51ce9c1
Jorge Johnson <jorge.johnson.code@gmail.com>
2020-08-21 21:03:05 -0500

    Update get_albums_from_artists.py

--- SQL/get_albums_from_artists.py
+++ SQL/get_albums_from_artists.py
@@ -13,3 +13,20 @@ con = sql.connect(sys.argv[1])
 cur = con.cursor()
 cur.execute("SELECT albums.Title From albums INNER JOIN artists ON albums.ArtistId = artists.ArtistId WHERE artists.name like (?)",("%"+sys.argv[2]+"%",))
 print(cur.fetchall())
+
+#no hay main
+#no hay verificación de  módulo __main__
+#no hay validaciones por posibilidad de error. mínimo un assert
+#los argumentos se deben recoger y validar.
+#construya las sentencia en una variable string antes de pasarla al execute
+#no se cerró la base de datos. ojo con esto!
+
+#mínimas funciones:
+#  main función de arranque
+#  getarguments para obtener los valores de los argumentos
+#  get_titles para obtener la lista de títulos
+#  print_titles para imprimir la lista de títulos
+
+#*******************************
+# deadline: 22 de Agosto.
+#*******************************
