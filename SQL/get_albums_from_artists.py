3103703b7bd0389a71a6d4c5a12058d20237075a
Maria Cadavid Velez <marycadavelez@gmail.com>
2020-08-22 23:36:36 -0500

    Upload corrected script

--- SQL/get_albums_from_artists.py
+++ SQL/get_albums_from_artists.py
@@ -6,27 +6,36 @@ Created on Tue Aug 18 19:47:15 2020
 @author: mariacadavid
 """
 
-#Al ejecutar desde python se deben incluir dos parametros: (1)path name de la base de datos, (2)nombre o nombre parcial del artista de interés
-import sys
-import sqlite3 as sql
-con = sql.connect(sys.argv[1])
-cur = con.cursor()
-cur.execute("SELECT albums.Title From albums INNER JOIN artists ON albums.ArtistId = artists.ArtistId WHERE artists.name like (?)",("%"+sys.argv[2]+"%",))
-print(cur.fetchall())
+#Al ejecutar fuera python se deben incluir dos parametros: (1)path name de la base de datos, (2)nombre o nombre parcial del artista de interés
 
-#no hay main
-#no hay verificación de  módulo __main__
-#no hay validaciones por posibilidad de error. mínimo un assert
-#los argumentos se deben recoger y validar.
-#construya las sentencia en una variable string antes de pasarla al execute
-#no se cerró la base de datos. ojo con esto!
+def get_arguments(args: list) -> list: 
+    list_variables= []
+    list_variables.append(args[1])
+    list_variables.append(args[2])
+    return(list_variables) 
+    
+def get_albums_titles(list_variables: list)-> list:
+    path= list_variables[0]
+    partial_name= list_variables[1]
+    import os 
+    assert os.path.isfile(path) , "archivo no existe"
+    import sqlite3 as sql
+    con = sql.connect(path)
+    cur = con.cursor()
+    query= "SELECT albums.Title From albums INNER JOIN artists ON albums.ArtistId = artists.ArtistId WHERE artists.name like (?)"
+    cur.execute(query,("%"+partial_name+"%",))
+    collection= (cur.fetchall())
+    con.close()
+    return(collection)
 
-#mínimas funciones:
-#  main función de arranque
-#  getarguments para obtener los valores de los argumentos
-#  get_titles para obtener la lista de títulos
-#  print_titles para imprimir la lista de títulos
-
-#*******************************
-# deadline: 22 de Agosto.
-#*******************************
+def print_titles(collection:list):
+    print (collection)
+    
+def main_fun(argv: list):
+    print_titles(get_albums_titles(get_arguments(argv)))
+    
+if __name__ == "__main__":
+    import sys
+    main_fun (sys.argv)
+    
+    
